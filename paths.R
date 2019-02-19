9c75b4fe2f0be7d8743da55bf6c9b671c0ceb590
bastianabaleiv <bastian.aballayleiva@gmail.com>
2019-02-19 20:53:31 -0300

    Read raw data

--- paths.R
+++ paths.R
@@ -1,6 +1,16 @@
 # Project -----------------------------------------------------------------
-wd.path <- '/media/sf_Google_Drive/Hospital_Forecast/Code/'
-data.path <- '/media/sf_Google_Drive/Hospital_Forecast/Data/'
-bd.path <- 'Data_Hospital.xlsx'
-fig.path <- '/media/sf_Google_Drive/Hospital_Forecast/Figures/'
+wd.path <- '/home/easymoney/Escritorio/hospadmis'
 setwd(wd.path)
+data.path <- '/home/easymoney/Escritorio/hospadmis/data'
+fig.path <- '/home/easymoney/Escritorio/hospadmis/figs'
+
+hospbd.path <- '/home/easymoney/Escritorio/hospadmis/data/01_Hospitales/'
+hosp.fpath <- '01_Raw_Hospitales.xlsx'
+
+sapubd.path <- '/home/easymoney/Escritorio/hospadmis/data/02_Sapu/'
+sapu.fpath <- '01_Raw_Sapu.xlsx'
+
+sarbd.path <- '/home/easymoney/Escritorio/hospadmis/data/03_Sar/'
+sar.fpath <- '01_Raw_Sar.xlsx'
+
+
--- reader.R
+++ reader.R
@@ -5,18 +5,24 @@ library('tsibble')
 source('paths.R')
 
 # CSV cache snapshot ------------------------------------------------------
+raw.files <- c(hosp = paste0(hospbd.path, hosp.fpath),
+                  sapu = paste0(sapubd.path, sapu.fpath),
+                  sar = paste0(sarbd.path, sar.fpath))
 
-resp.raw <- paste0(data.path, bd.path) %>%
-    read_excel(sheet = 'Página1_1',
-               range = 'B17:DHK19',
-               col_names = FALSE) %>% t()
+resp.raw <-  raw.files[1] %>% read_excel(sheet = 'Página1_1',
+                        range = 'B17:BRG19',
+                        col_names = FALSE) %>% t()
 
-resp <-
-    tsibble(admis = as.numeric(resp.raw[, 3]),
-        date = as.Date(resp.raw[, 1]),
+resp.raw <- lapply(raw.files, function(x) read_excel(x, sheet = 'Página1_1',
+                                                     range = 'B17:BRG19',
+                                                     col_names = FALSE) %>% t())
+
+resp <- lapply(resp.raw, function(x)
+    tsibble(admis = as.numeric(x[, 3]),
+        date = as.Date(x[, 1]),
         key = id(),
         index = date
-    )
+    ))
 
 resp %>% 
    write_csv(paste(data.path, 'resp_disease.csv'))
